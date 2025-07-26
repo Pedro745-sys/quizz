e2068d9daf347b2422e588604b9b102d6af3ef5c
Pedro <pedro14ucas@gmail.com>
2025-07-26 16:51:37 -0300

    Add files via upload

---
+++ Quiz/main.js
@@ -0,0 +1,24 @@
+function vperg1(botaoClicado, opcaoSelecionada){
+    if(opcaoSelecionada == 'c1'){
+        var respCerta = document.getElementById('c1');
+        respCerta.style.backgroundColor = "green";
+        document.getElementById('resp1').innerHTML= "✅Parabéns você acertou!"
+    }else{
+        var respCerta = document.getElementById('c1');
+        respCerta.style.backgroundColor = "green";
+        botaoClicado.style.backgroundColor = "red";
+        document.getElementById('resp1').innerHTML="❌ops errou!"
+    }
+}
+function vperg2(botaoClicado2,opcaoSelecionada2){
+    if(opcaoSelecionada2 == b2){
+        var respCerta = document.getElementById('b2');
+        respCerta.style.backgroundColor = "green";
+        document.getElementById('resp2').innerHTML = "✅Parabéns você acertou!"
+    }else{
+        var respCerta = document.getElementById('b2');
+        respCerta.style.backgroundColor = "green";
+        botaoClicado.style.backgroundColor = "red";
+        document.getElementById('resp2').innerHTML="❌ops errou!"
+    }
+}
